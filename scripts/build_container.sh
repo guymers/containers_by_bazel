e0fbc7123460b098338da56d632594d5a4cf6ce4
Sam Guymer <sam@guymer.me>
2016-02-13 21:59:51 +1000

    Update build container script
    If no version is provided use the first 10 characters of the images id.

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -15,27 +15,14 @@ version="$3"
 bazel build --verbose_failures "$bazel_target"
 
 run_result=$(bazel run "$bazel_target" | grep "^Tagging")
-local_image_sha=$(echo "$run_result" | awk '{print $2}')
+local_image_id=$(echo "$run_result" | awk '{print $2}')
 remote_image="$DOCKER_REGISTRY/$container_name"
 
 echo "Built $(echo "$run_result" | awk '{print $4}')"
 
-# if there is no version provided then generate one from the git
-# commit + current timestamp
+# if there is no version provided then generate one from the container id
 if [ -z "$version" ]; then
-  if [ "$DOCKER_REGISTRY" != "$DEFAULT_REGISTRY" ]; then
-    docker pull "$remote_image" 2> /dev/null || true
-  fi
-  latest_sha=$(docker inspect --format="{{ .Id }}" "$remote_image" 2> /dev/null || echo "")
-
-  if [ "$local_image_sha" = "$latest_sha" ]; then
-    echo "Image $remote_image has not changed" >&2
-    exit 0
-  fi
-
-  TIMESTAMP=$(date -u +"%Y%m%d%H%M%S")
-  GIT_COMMIT=${GIT_COMMIT:-$(git rev-parse --short HEAD)}
-  version="${TIMESTAMP}-${GIT_COMMIT:0:7}"
+  version="${local_image_id:0:10}"
 fi
 
 remote_image_version="$remote_image:$version"
@@ -45,20 +32,20 @@ if [ "$DOCKER_REGISTRY" != "$DEFAULT_REGISTRY" ]; then
   docker pull "$remote_image_version" 2> /dev/null || true
 fi
 
-current_sha=$(docker inspect --format="{{ .Id }}" "$remote_image_version" 2> /dev/null || echo "")
+current_id=$(docker inspect --format="{{ .Id }}" "$remote_image_version" 2> /dev/null || echo "")
 
-if [ "$local_image_sha" == "$current_sha" ]; then
+if [ "$local_image_id" == "$current_id" ]; then
   echo "Image has not changed $remote_image $version"
   exit 0
 fi
 
 # enforce docker tag immutability
-if [ -n "$current_sha" ] && [ "$local_image_sha" != "$current_sha" ]; then
-  echo "Attempting to update $remote_image_version to new hash $local_image_sha" >&2
+if [ -n "$current_id" ] && [ "$local_image_id" != "$current_id" ]; then
+  echo "Attempting to update $remote_image_version to new hash $local_image_id" >&2
   exit 1
 fi
 
-docker tag -f "$local_image_sha" "$remote_image_version"
+docker tag -f "$local_image_id" "$remote_image_version"
 
 echo "Created $remote_image $version"
 exit 0
