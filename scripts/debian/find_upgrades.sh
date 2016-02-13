e3f77bf557aa768b8ec0748eae496d2a63680242
Sam Guymer <sam@guymer.me>
2016-02-13 22:01:23 +1000

    Add dependency upgrade script

---
+++ scripts/debian/find_upgrades.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set -o pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$DIR/../versions.sh"
+
+dependencies=$(
+  for file in $DIR/dependencies/*; do
+    if [ -f "$file" ] && [ $(basename "$file") != "nodejs" ]; then
+      while IFS="=" read dependency version; do
+        # yes eval, easiest way to substitute variables
+        version=$(eval echo "$version")
+        echo "$dependency=$version"
+      done < "$file"
+    fi
+  done
+)
+dependencies=$(echo "$dependencies" | tr '\r\n' ' ')
+
+container="bazel-container/dependencies-base"
+cmd="apt-get install --no-install-recommends --yes $dependencies && apt-get update && apt-get -u upgrade --assume-no"
+docker run --rm "$container" bash -c "$cmd"
