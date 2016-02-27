d630c39aa497f02501dc39cdd075e659fae7b72b
Sam Guymer <sam@guymer.me>
2016-02-27 21:17:11 +1000

    Update immutable image checking
    Docker image ids are random until 1.10

--- scripts/build_container.sh
+++ scripts/build_container.sh
@@ -23,27 +23,19 @@ echo "Built $(echo "$run_result" | awk '{print $4}')"
 # if there is no version provided then generate one from the container id
 if [ -z "$version" ]; then
   version="${local_image_id:0:12}"
+
+  remote_image_version="$remote_image:$version"
+
+  # docker image ids are not deterministic until 1.10 so just check if an image exists with the tag
+  if [ "$DOCKER_REGISTRY" != "$DEFAULT_REGISTRY" ]; then
+    if docker pull "$remote_image_version" 2> /dev/null; then
+      echo "Image has not changed $remote_image $version"
+      exit 0
+    fi
+  fi
 fi
 
 remote_image_version="$remote_image:$version"
-
-# make sure the remote image is up to date
-if [ "$DOCKER_REGISTRY" != "$DEFAULT_REGISTRY" ]; then
-  docker pull "$remote_image_version" 2> /dev/null || true
-fi
-
-current_id=$(docker inspect --format="{{ .Id }}" "$remote_image_version" 2> /dev/null || echo "")
-
-if [ "$local_image_id" == "$current_id" ]; then
-  echo "Image has not changed $remote_image $version"
-  exit 0
-fi
-
-# enforce docker tag immutability
-if [ -n "$current_id" ] && [ "$local_image_id" != "$current_id" ]; then
-  echo "Attempting to update $remote_image_version to new hash $local_image_id" >&2
-  exit 1
-fi
 
 docker tag -f "$local_image_id" "$remote_image_version"
 
