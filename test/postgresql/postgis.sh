306ce65e298319e976cfe535950c42453ad1f9ef
Sam Guymer <sam@guymer.me>
2016-05-07 23:02:08 +1000

    Possible CircleCI postgresql test fix

--- test/postgresql/postgis.sh
+++ test/postgresql/postgis.sh
@@ -5,9 +5,10 @@ while ! psql -U postgres &>/dev/null; do :; done
 sleep 5
 while ! psql -U postgres &>/dev/null; do :; done
 
-psql -U postgres -c "CREATE DATABASE test"
-psql -U postgres -d test -c "CREATE EXTENSION IF NOT EXISTS postgis"
-psql -U postgres -d test -c "CREATE TABLE test_table (test_table_id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
-psql -U postgres -d test -c "SELECT AddGeometryColumn('test_table', 'geom', '4326', 'POINT', 2)" > /dev/null
-psql -U postgres -d test -c "INSERT INTO test_table (name, geom) VALUES ('blah', ST_PointFromText('POINT(-71.064544 42.28787)', 4326))"
-psql -U postgres -d test -c 'SELECT * FROM test_table ORDER BY name' | sed 's/ *$//g'
+readonly psql="psql -h 127.0.0.1 -U postgres"
+$psql -c "CREATE DATABASE test"
+$psql -d test -c "CREATE EXTENSION IF NOT EXISTS postgis"
+$psql -d test -c "CREATE TABLE test_table (test_table_id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+$psql -d test -c "SELECT AddGeometryColumn('test_table', 'geom', '4326', 'POINT', 2)" > /dev/null
+$psql -d test -c "INSERT INTO test_table (name, geom) VALUES ('blah', ST_PointFromText('POINT(-71.064544 42.28787)', 4326))"
+$psql -d test -c 'SELECT * FROM test_table ORDER BY name' | sed 's/ *$//g'
--- test/postgresql/postgresql.sh
+++ test/postgresql/postgresql.sh
@@ -5,7 +5,8 @@ while ! psql -U postgres &>/dev/null; do :; done
 sleep 5
 while ! psql -U postgres &>/dev/null; do :; done
 
-psql -U postgres -c "CREATE DATABASE test"
-psql -U postgres -d test -c "CREATE TABLE test_table (test_table_id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
-psql -U postgres -d test -c "INSERT INTO test_table (name) VALUES ('blah')"
-psql -U postgres -d test -c 'SELECT * FROM test_table ORDER BY name' | sed 's/ *$//g'
+readonly psql="psql -h 127.0.0.1 -U postgres"
+$psql -c "CREATE DATABASE test"
+$psql -d test -c "CREATE TABLE test_table (test_table_id SERIAL PRIMARY KEY, name TEXT NOT NULL)"
+$psql -d test -c "INSERT INTO test_table (name) VALUES ('blah')"
+$psql -d test -c 'SELECT * FROM test_table ORDER BY name' | sed 's/ *$//g'
