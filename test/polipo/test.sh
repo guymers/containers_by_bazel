bfe4f8c40f260ccefe5185b7f121a205397e6926
Sam Guymer <sam@guymer.me>
2016-05-13 23:54:48 +1000

    polipo test

---
+++ test/polipo/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+apt-get update > /dev/null
+apt-get install -y wget > /dev/null
+
+export http_proxy=http://localhost:8123
+readonly url=http://mirror.internode.on.net/pub/test/1meg.test
+cd /tmp
+readonly log=$(wget --retry-connrefused --waitretry=10 --timeout=20 --tries=10 "$url" 2>&1)
+echo "$log" | grep "Connecting to localhost (localhost)|::1|:8123... connected."
+echo "$log" | grep "Proxy request sent, awaiting response... 200 OK"
+sha256sum 1meg.test
