9bc72b4b770f4bfea7f2807f5eb3797b47febfa5
Sam Guymer <sam@guymer.me>
2016-08-07 23:08:52 +1000

    Move to Travis CI
    Travis CI allows an offical Docker installation.

--- test/polipo/test.sh
+++ test/polipo/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
 export http_proxy=http://localhost:8123
-readonly url=http://mirror.internode.on.net/pub/test/1meg.test
+export https_proxy=http://localhost:8123
+readonly url=https://github.com/krallin/tini/releases/download/v0.9.0/tini_0.9.0.deb
 cd /tmp
 readonly log=$(wget --retry-connrefused --waitretry=10 --timeout=20 --tries=10 "$url" 2>&1)
-echo "$log" | grep "Connecting to localhost (localhost)|::1|:8123... connected."
+echo "$log" | grep -m 1 "Connecting to localhost (localhost)|::1|:8123... connected."
 echo "$log" | grep "Proxy request sent, awaiting response... 200 OK"
-sha256sum 1meg.test
+sha256sum tini_0.9.0.deb
